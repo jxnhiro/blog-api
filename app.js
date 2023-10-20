0b82f166f552e1ef1b29c6257a1e9d3193099ad3
Kenny Jinhiro <jxnhiro@gmail.com>
2023-10-20 18:53:14 +0700

    Added Error Handling

--- app.js
+++ app.js
@@ -1,25 +1,39 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const env = require('dotenv').config();
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const env = require("dotenv").config();
+const cors = require("cors");
+const path = require("path");
 
-const feedRoutes = require('./routes/feed');
+const feedRoutes = require("./routes/feed");
+const { watch } = require("./models/post");
 const MONGO_URL = process.env.MONGODB_URI;
 const app = express();
 
-app.use(bodyParser.json());
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use(cors());
+app.use(bodyParser.json());
 
-app.use('/feed', feedRoutes);
+app.use("/feed", feedRoutes);
 
+app.use((error, req, res, next) => {
+  console.log(`Error: ${error}`);
+
+  const statusCode = error.statusCode || 500;
+  const message = error.message;
+
+  res.status(statusCode).json({
+    message: `Error ${statusCode}`,
+    description: message,
+  });
+});
 mongoose
   .connect(MONGO_URL)
   .then(() => {
     app.listen(8080);
-    console.log('Connected to MongoDB Database');
+    console.log("Connected to MongoDB Database");
   })
   .catch((err) => {
-    console.log('Error Found in App.JS: ', err);
+    console.log("Error Found in App.JS: ", err);
   });
