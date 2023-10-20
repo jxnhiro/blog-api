2916982f92746124a0059e87eda72a356787eecc
Kenny Jinhiro <jxnhiro@gmail.com>
2023-10-20 17:59:39 +0700

    Added POST method and validation for Posts

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const env = require('dotenv').config();
+const cors = require('cors');
 
 const feedRoutes = require('./routes/feed');
-
+const MONGO_URL = process.env.MONGODB_URI;
 const app = express();
 
 app.use(bodyParser.json());
 
+app.use(cors());
+
 app.use('/feed', feedRoutes);
 
-app.listen(8080);
+mongoose
+  .connect(MONGO_URL)
+  .then(() => {
+    app.listen(8080);
+    console.log('Connected to MongoDB Database');
+  })
+  .catch((err) => {
+    console.log('Error Found in App.JS: ', err);
+  });
--- controllers/feed.js
+++ controllers/feed.js
@@ -1,17 +1,47 @@
+const { validationResult } = require('express-validator');
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
-    title: "This is your first content",
-    content: "This is your content"
+    posts: [
+      {
+        _id: '1',
+        title: 'This is your first content',
+        content: 'This is your content',
+        imageUrl: 'images/duck.jpg',
+        creator: {
+          name: 'Maximillian',
+        },
+        createdAt: new Date(),
+      }
+    ]
   });
 };
 
 exports.createPost = (req, res, next) => {
+
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      message: 'Validation failed!',
+      description: 'Entered data has the incorrect format.',
+      errors: errors.array()
+    });
+  }
+
   const title = req.body.title;
   const content = req.body.content;
 
   res.status(201).json({
-    message: "Successfully created title!",
-    title: title,
-    content: content
+    message: 'Post created successfully!',
+    post: {
+      _id: new Date().toISOString(),
+      title: title,
+      content: content,
+      creator: {
+        name: 'Maximillian'
+      },
+      createdAt: new Date(),
+    }
   });
 };
