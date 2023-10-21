d676d0092c60dd9443dcb805023a121f779f7ba7
Kenny Jinhiro <jxnhiro@gmail.com>
2023-10-21 12:09:39 +0700

    Added delete endpoint

--- controllers/feed.js
+++ controllers/feed.js
@@ -130,3 +130,33 @@ exports.updatePost = (req, res, next) => {
       utilities.checkForStatusCode(err);
     });
 };
+
+exports.deletePost = (req, res, next) => {
+  const postId = req.params.postId;
+
+  Post.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const error = new Error("Post not found.");
+        error.statusCode = 404;
+        throw error;
+      }
+
+      //TODO: Check for logged in user.
+
+      utilities.clearImage(post.imageUrl);
+
+      return Post.findByIdAndRemove(postId);
+    })
+    .then((result) => {
+      console.log(result);
+
+      res.status(200).json({
+        message: "Successfully deleted post",
+        descrption: result,
+      });
+    })
+    .catch((err) => {
+      utilities.checkForStatusCode(err);
+    });
+};
--- routes/feed.js
+++ routes/feed.js
@@ -27,4 +27,6 @@ router.post(
   feedController.createPost,
 );
 
+router.delete("/post/:postId", feedController.deletePost);
+
 module.exports = router;
